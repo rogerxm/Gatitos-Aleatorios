74cefa1796f9624422b03a408e37e6df442b40d6
Roger Montes <rogerxmontes@gmail.com>
2022-08-15 12:04:18 -0500

    agregué el button al js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-console.log('Hello world')
-
 const url = 'https://api.thecatapi.com/v1/images/search';
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
-    });
+async function getCat() {
+    const response = await fetch(url);
+    const data = await response.json();
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+}
+const button = document.querySelector('input');
+button.onclick = getCat;   
